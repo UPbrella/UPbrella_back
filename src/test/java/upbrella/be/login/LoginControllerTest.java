d536d83e7bf6fc8e730c2abc419fae73c535f4f5
birdieHyun <flaehdan@gmail.com>
2023-07-15 06:05:09 +0900

    feat: OAuth 를 활용한 로그인 API 명세서 작성

---
+++ src/test/java/upbrella/be/login/LoginControllerTest.java
@@ -0,0 +1,106 @@
+package upbrella.be.login;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
+import upbrella.be.docs.RestDocsSupport;
+import upbrella.be.login.controller.LoginController;
+import upbrella.be.login.dto.response.LoggedInUserResponse;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class LoginControllerTest extends RestDocsSupport {
+    @Override
+    protected Object initController() {
+        return new LoginController();
+    }
+
+    @Test
+    @DisplayName("사용자는 카카오 소셜 로그인을 할 수 있다.")
+    void kakoLoginTest() throws Exception {
+        // given
+        LoggedInUserResponse.builder()
+                .id(1L)
+                .socialId(1L)
+                .name("카카오 사용자")
+                .phoneNumber("010-0000-0000")
+                .adminStatus(false)
+                .build();
+
+        // when
+        mockMvc.perform(
+                        get("/oauth/kakao")
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("kakao-login-doc",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("데이터"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER)
+                                        .description("사용자 식별자"),
+                                fieldWithPath("data.socialId").type(JsonFieldType.NUMBER)
+                                        .description("사용자 소셜 식별자"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING)
+                                        .description("사용자 이름"),
+                                fieldWithPath("data.phoneNumber").type(JsonFieldType.STRING)
+                                        .description("사용자 전화번호"),
+                                fieldWithPath("data.adminStatus").type(JsonFieldType.BOOLEAN)
+                                        .description("관리자 여부")
+                                )));
+    }
+
+    @Test
+    @DisplayName("사용자는 네이버 소셜 로그인을 할 수 있다.")
+    void naverLoginTest() throws Exception {
+        // given
+        LoggedInUserResponse.builder()
+                .id(1L)
+                .socialId(1L)
+                .name("네이버 사용자")
+                .phoneNumber("010-0000-0000")
+                .adminStatus(false)
+                .build();
+
+        // when
+        mockMvc.perform(
+                        get("/oauth/naver")
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("naver-login-doc",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("데이터"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER)
+                                        .description("사용자 식별자"),
+                                fieldWithPath("data.socialId").type(JsonFieldType.NUMBER)
+                                        .description("사용자 소셜 식별자"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING)
+                                        .description("사용자 이름"),
+                                fieldWithPath("data.phoneNumber").type(JsonFieldType.STRING)
+                                        .description("사용자 전화번호"),
+                                fieldWithPath("data.adminStatus").type(JsonFieldType.BOOLEAN)
+                                        .description("관리자 여부")
+                        )));
+    }
+}
