c8fe2eccc056dc7055939f876c1a8256e0f71732
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-14 17:40:21 +0900

    feat: 우산 대여 폼 작성 api에 대한 문서 작성을 위한 정보 생성

---
+++ src/main/java/upbrella/be/rent/controller/RentController.java
@@ -0,0 +1,26 @@
+package upbrella.be.rent.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import upbrella.be.user.dto.response.UserInfoResponse;
+import upbrella.be.util.CustomResponse;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/rent")
+public class RentController {
+
+    @PostMapping
+    public ResponseEntity<CustomResponse> findUserInfo(HttpSession httpSession) {
+
+        return ResponseEntity
+                .ok()
+                .body(new CustomResponse(
+                        "success",
+                        200,
+                        "우산 대여 성공"
+                ));
+    }
+}
