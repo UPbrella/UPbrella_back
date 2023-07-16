7b958de680374c696add527e106adca60755246c
birdieHyun <flaehdan@gmail.com>
2023-07-16 10:13:39 +0900

    refactor: 요구사항 변경에 따른 협업지점 등록 api 명세서 수정

--- src/main/java/upbrella/be/store/dto/request/CreateStoreRequest.java
+++ src/main/java/upbrella/be/store/dto/request/CreateStoreRequest.java
@@ -1,17 +1,16 @@
 package upbrella.be.store.dto.request;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CreateStoreRequest {
 
-    private long id;
     private String name;
     private String classification;
     private boolean activateStatus;
@@ -21,5 +20,5 @@ public class CreateStoreRequest {
     private String contactNumber;
     private String instagramId;
     private String coordinate; //  좌표를 줘서 프론에서 네이버 지도 api를 이용해서 좌표로 지도를 띄워줄 수 있도록
-    private List<MultipartFile> images;
+    private List<String> imageUrls;
 }
