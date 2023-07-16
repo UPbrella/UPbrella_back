e1a2cfa9ba2f93957975011270c6236ae8e54263
birdieHyun <flaehdan@gmail.com>
2023-07-16 15:16:31 +0900

    feat: 협업지점 수정,삭제 API 추가

--- src/main/java/upbrella/be/store/dto/request/UpdateStoreRequest.java
+++ src/main/java/upbrella/be/store/dto/request/UpdateStoreRequest.java
@@ -2,7 +2,6 @@ package upbrella.be.store.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ import java.util.List;
 @Builder
 public class UpdateStoreRequest {
 
-    private long id;
     private String name;
     private String classification;
     private boolean activateStatus;
@@ -20,5 +18,5 @@ public class UpdateStoreRequest {
     private String contactNumber;
     private String instagramId;
     private String coordinate; //  좌표를 줘서 프론에서 네이버 지도 api를 이용해서 좌표로 지도를 띄워줄 수 있도록
-    private List<MultipartFile> images;
+    private List<String> imageUrls;
 }
