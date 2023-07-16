940e773e681c7447c983de960dd9a833f95cdf0c
birdieHyun <flaehdan@gmail.com>
2023-07-16 10:13:39 +0900

    refactor: 요구사항 변경에 따른 협업지점 등록 api 명세서 수정

--- src/test/java/upbrella/be/store/controller/StoreControllerTest.java
+++ src/test/java/upbrella/be/store/controller/StoreControllerTest.java
@@ -2,9 +2,13 @@ package upbrella.be.store.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.payload.JsonFieldType;
 import upbrella.be.docs.RestDocsSupport;
+import upbrella.be.store.dto.request.CreateStoreRequest;
+
+import java.util.List;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -163,46 +167,56 @@ class StoreControllerTest extends RestDocsSupport {
     @DisplayName("관리자는 새로운 협업지점을 등록할 수 있다.")
     void createStoreTest() throws Exception {
         // given
-
+        CreateStoreRequest store = CreateStoreRequest.builder()
+                .name("협업지점명")
+                .classification("분류")
+                .activateStatus(true)
+                .address("주소")
+                .umbrellaLocation("우산 위치")
+                .businessHours("영업시간")
+                .contactNumber("연락처")
+                .instagramId("인스타그램 아이디")
+                .coordinate("네이버 길찾기를 위한 좌표")
+                .imageUrls(List.of("이미지 URL"))
+                .build();
 
         // when
+        System.out.println("store.isActivateStatus = " + store.isActivateStatus());
 
-
+        String s = objectMapper.writeValueAsString(store);
+        System.out.println("s = " + s);
         // then
-        MockMultipartFile imageFile = new MockMultipartFile(
-                "images", "sample.jpg", "image/jpeg", "sample-image".getBytes());
 
-        mockMvc.perform(multipart("/stores")
-                        .file(imageFile)
-                        .param("id", "1")
-                        .param("name", "스타벅스")
-                        .param("classification", "카페")
-                        .param("activateStatus", "true")
-                        .param("address", "서울시 강남구")
-                        .param("umbrellaLocation", "1층")
-                        .param("businessHours", "10:00 ~ 22:00")
-                        .param("contactNumber", "010-1234-5678")
-                        .param("instagramId", "starbucks")
-                        .param("coordinate", "127.123, 37.123"))
+        mockMvc.perform(post("/stores")
+                        .content(objectMapper.writeValueAsString(store))
+                        .contentType(MediaType.APPLICATION_JSON)
+                       )
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andDo(document("store-create-doc",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
-                        requestParts(
-                                partWithName("images").description("이미지 파일")
-                        ),
-                        requestParameters(
-                                parameterWithName("id").description("아이디"),
-                                parameterWithName("name").description("협업지점명"),
-                                parameterWithName("classification").description("분류"),
-                                parameterWithName("activateStatus").description("활성화 여부"),
-                                parameterWithName("address").description("주소"),
-                                parameterWithName("umbrellaLocation").description("우산 위치"),
-                                parameterWithName("businessHours").description("영업시간"),
-                                parameterWithName("contactNumber").description("연락처"),
-                                parameterWithName("instagramId").description("인스타그램 아이디"),
-                                parameterWithName("coordinate").description("네이버 길찾기를 위한 좌표")
+                        requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING)
+                                        .description("협업지점명"),
+                                fieldWithPath("classification").type(JsonFieldType.STRING)
+                                        .description("분류"),
+                                fieldWithPath("activateStatus").type(JsonFieldType.BOOLEAN)
+                                        .description("활성화 여부"),
+                                fieldWithPath("address").type(JsonFieldType.STRING)
+                                        .description("주소"),
+                                fieldWithPath("umbrellaLocation").type(JsonFieldType.STRING)
+                                        .description("우산 위치"),
+                                fieldWithPath("businessHours").type(JsonFieldType.STRING)
+                                        .description("영업시간"),
+                                fieldWithPath("contactNumber").type(JsonFieldType.STRING)
+                                        .description("연락처"),
+                                fieldWithPath("instagramId").type(JsonFieldType.STRING)
+                                        .description("인스타그램 아이디"),
+                                fieldWithPath("coordinate").type(JsonFieldType.STRING)
+                                        .description("네이버 길찾기를 위한 좌표"),
+                                fieldWithPath("imageUrls[]").type(JsonFieldType.ARRAY)
+                                        .description("이미지 URL")
                         ),
                         responseFields(
                                 fieldWithPath("code").type(JsonFieldType.NUMBER)
@@ -216,20 +230,15 @@ class StoreControllerTest extends RestDocsSupport {
                         )));
     }
 
-    // TODO: put 어떻게 처리할지 고민해보기 1. 이미지 API 변경,
-    @Test
+    // TODO: patch 어떻게 처리할지 고민해보기 1. 이미지 API 변경,
+//    @Test
     @DisplayName("관리자는 협업지점 정보를 수정할 수 있다.")
     void updateStoreTest() throws Exception {
         // given
-
-
-        // when
-
-
-        // then
         MockMultipartFile imageFile = new MockMultipartFile(
                 "images", "sample.jpg", "image/jpeg", "sample-image".getBytes());
 
+        // when & then
         mockMvc.perform(multipart("/stores/{storedId}", 1L)
                         .file(imageFile)
                         .param("id", "1")
@@ -274,6 +283,38 @@ class StoreControllerTest extends RestDocsSupport {
                                         .description("메시지"),
                                 fieldWithPath("data").type(JsonFieldType.NULL)
                                         .description("데이터 값이 없습니다.")
+                        )));
+    }
+
+    @Test
+    @DisplayName("사용자는 협업지점의 사진을 등록해서 사진의 url을 받을 수 있다.")
+    void uploadStorageImages() throws Exception {
+        // given
+        MockMultipartFile firstFile = new MockMultipartFile("files", "filename-1.jpeg", "text/plain", "some-image".getBytes());
+        MockMultipartFile secondFile = new MockMultipartFile("files", "filename-2.jpeg", "text/plain", "some-image".getBytes());
+
+        // when
+
+        // then
+        mockMvc.perform(multipart("/stores/images")
+                        .file(firstFile)
+                        .file(secondFile))
+                .andExpect(status().isOk())
+                .andDo(document("upload-file",
+                        requestParts(
+                                partWithName("files").description("The files to upload")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("데이터"),
+                                fieldWithPath("data.imageUrls[]").type(JsonFieldType.ARRAY)
+                                        .description("이미지 url")
                         )));
     }
 }
