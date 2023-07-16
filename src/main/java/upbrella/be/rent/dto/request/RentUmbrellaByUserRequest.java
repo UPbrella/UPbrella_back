d81cac6ffc56324b896f773cd843e81e3e87d3b9
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-16 21:50:25 +0900

    feat: 우산 대여 요청 API REST Docs 문서를 위한 코드 작성(#12)

--- src/main/java/upbrella/be/rent/dto/request/RentUmbrellaByUserRequest.java
+++ src/main/java/upbrella/be/rent/dto/request/RentUmbrellaByUserRequest.java
@@ -1,9 +1,13 @@
 package upbrella.be.rent.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class RentUmbrellaByUserRequest {
 
