97f1df5922f31760d3e5560d198731fe40f98924
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-17 17:40:50 +0900

    feat: 우산 반납 API REST Docs 작성 (#12)

--- src/test/java/upbrella/be/rent/controller/RentControllerTest.java
+++ src/test/java/upbrella/be/rent/controller/RentControllerTest.java
@@ -6,10 +6,12 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import upbrella.be.docs.RestDocsSupport;
 import upbrella.be.rent.dto.request.RentUmbrellaByUserRequest;
+import upbrella.be.rent.dto.request.ReturnUmbrellaByUserRequest;
 import upbrella.be.rent.service.RentService;
 
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -33,6 +35,7 @@ public class RentControllerTest extends RestDocsSupport {
                 .region("신촌")
                 .storeId(1)
                 .umbrellaId(1)
+                .statusDeclaration("필요하다면 상태 신고를 해주세요.")
                 .build();
 
         mockMvc.perform(
@@ -55,6 +58,46 @@ public class RentControllerTest extends RestDocsSupport {
                                 fieldWithPath("statusDeclaration").type(JsonFieldType.STRING)
                                         .optional()
                                         .description("상태 신고")),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.NULL)
+                                        .description("데이터 값이 없습니다.")
+                        )));
+    }
+
+    @DisplayName("사용자는 우산 대여 요청을 할 수 있다.")
+    @Test
+    void returnUmbrella() throws Exception {
+        ReturnUmbrellaByUserRequest request = ReturnUmbrellaByUserRequest.builder()
+                .umbrellaId(1)
+                .storeId(1)
+                .improvement("불편하셨다면 개선 사항을 입력해주세요.")
+                .build();
+
+
+        mockMvc.perform(
+                        patch("/rents")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("return-umbrella-doc",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("umbrellaId").type(JsonFieldType.NUMBER)
+                                        .description("우산 아이디"),
+                                fieldWithPath("storeId").type(JsonFieldType.NUMBER)
+                                        .description("대여점 아이디"),
+                                fieldWithPath("improvement").type(JsonFieldType.STRING)
+                                        .optional()
+                                        .description("개선 사항")),
                         responseFields(
                                 fieldWithPath("code").type(JsonFieldType.NUMBER)
                                         .description("코드"),
