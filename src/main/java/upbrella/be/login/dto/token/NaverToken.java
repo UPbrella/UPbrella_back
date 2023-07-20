a7a68bf1f9644fe9385a7d4899c9c3ffc58aa38e
birdieHyun <flaehdan@gmail.com>
2023-07-20 14:05:44 +0900

    refactor: JsonNaming 클래스 레벨에서 처리 (#35)

--- src/main/java/upbrella/be/login/dto/token/NaverToken.java
+++ src/main/java/upbrella/be/login/dto/token/NaverToken.java
@@ -1,21 +1,18 @@
 package upbrella.be.login.dto.token;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class NaverToken {
 
-    @JsonProperty("access_token")
     private String accessToken;
-    @JsonProperty("refresh_token")
     private String refreshToken;
-    @JsonProperty("token_type")
     private String tokenType;
-    @JsonProperty("expires_in")
     private Long expiresIn;
 }
