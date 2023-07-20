881aad2d534b150b54590e5a1a819b3ba25f28b8
gwonwoo-nam <ngw7617@naver.com>
2023-07-20 14:39:18 +0900

    feat: 우산 추가 기능 구현

--- src/main/java/upbrella/be/umbrella/entity/Umbrella.java
+++ src/main/java/upbrella/be/umbrella/entity/Umbrella.java
@@ -1,21 +1,30 @@
 package upbrella.be.umbrella.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import upbrella.be.store.entity.StoreMeta;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Umbrella {
 
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @ManyToOne
     @JoinColumn(name = "store_meta_id")
     private StoreMeta storeMeta;
     private long uuid;
     private boolean rentable;
     private boolean deleted;
+
+    public static Umbrella createOf(StoreMeta storeMeta, long uuid, boolean rentable) {
+
+        return new Umbrella(null, storeMeta, uuid, rentable, false);
+    }
 }
--- src/main/java/upbrella/be/umbrella/service/UmbrellaService.java
+++ src/main/java/upbrella/be/umbrella/service/UmbrellaService.java
@@ -1,13 +1,21 @@
 package upbrella.be.umbrella.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import upbrella.be.store.StoreRepository.StoreMetaRepository;
+import upbrella.be.store.entity.StoreMeta;
 import upbrella.be.umbrella.dto.request.UmbrellaRequest;
 import upbrella.be.umbrella.dto.response.UmbrellaResponse;
+import upbrella.be.umbrella.entity.Umbrella;
+import upbrella.be.umbrella.repository.UmbrellaRepository;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UmbrellaService {
+    private final UmbrellaRepository umbrellaRepository;
+    private final StoreMetaRepository storeMetaRepository;
     public List<UmbrellaResponse> findAllUmbrellas() {
         return null;
     }
@@ -15,8 +23,26 @@ public class UmbrellaService {
         return null;
     }
 
-    public void addUmbrella(UmbrellaRequest umbrellaRequest) {}
-    public void modifyUmbrella(long id, UmbrellaRequest umbrellaRequest) {}
+    public void addUmbrella(UmbrellaRequest umbrellaRequest) {
+
+        StoreMeta storeMeta = storeMetaRepository.findById(umbrellaRequest.getStoreMetaId())
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 협업 지점 고유번호입니다."));
+
+        if (umbrellaRepository.existsByUuid(umbrellaRequest.getUuid())) {
+            throw new IllegalArgumentException("[ERROR] 이미 존재하는 우산 관리 번호입니다.");
+        }
+
+        Umbrella umbrella = Umbrella.createOf(
+                storeMeta,
+                umbrellaRequest.getUuid(),
+                umbrellaRequest.isRentable()
+        );
+
+        umbrellaRepository.save(umbrella);
+    }
+    public void modifyUmbrella(long id, UmbrellaRequest umbrellaRequest) {
+
+    }
     public void deleteUmbrella(long id) {}
 
 }
