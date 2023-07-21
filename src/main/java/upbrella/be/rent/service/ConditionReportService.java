f08b58fc5b3f35e05d5a1e38baeb64514654ead6
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-21 23:58:43 +0900

    feat: JPA 사용 시 하나의 작업으로 처리해주기 위해 Transactional 어노테이션 추가(#48)

--- src/main/java/upbrella/be/rent/service/ConditionReportService.java
+++ src/main/java/upbrella/be/rent/service/ConditionReportService.java
@@ -2,6 +2,7 @@ package upbrella.be.rent.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import upbrella.be.rent.dto.response.ConditionReportResponse;
 import upbrella.be.rent.repository.ConditionReportRepository;
 
@@ -14,6 +15,7 @@ public class ConditionReportService {
 
     private final ConditionReportRepository conditionReportRepository;
 
+    @Transactional
     public List<ConditionReportResponse> findAllConditionReport() {
         return conditionReportRepository.findAll().stream()
                 .map(conditionReport -> new ConditionReportResponse().builder()
--- src/main/java/upbrella/be/rent/service/RentService.java
+++ src/main/java/upbrella/be/rent/service/RentService.java
@@ -1,9 +1,11 @@
 package upbrella.be.rent.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import upbrella.be.rent.dto.request.RentUmbrellaByUserRequest;
 
 public class RentService {
 
+    @Transactional
     public void validRent(RentUmbrellaByUserRequest rentUmbrellaByUserRequest) {
 
     }
