2b22a95ac87eab8dd5451b4426167fcd5dad9a4d
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-21 23:28:44 +0900

    feat: 상태 신고 repository 생성(#48)

---
+++ src/main/java/upbrella/be/rent/repository/ConditionReportRepository.java
@@ -0,0 +1,11 @@
+package upbrella.be.rent.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import upbrella.be.rent.entity.ConditionReport;
+
+import java.util.List;
+
+public interface ConditionReportRepository extends JpaRepository<ConditionReport, Long> {
+
+    List<ConditionReport>  findAllConditionReport();
+}
