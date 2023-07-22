d10601bc3843d57bbfb11f680631f1ab6d01f3a8
birdieHyun <flaehdan@gmail.com>
2023-07-22 12:27:55 +0900

    refactor: 사용하지 않는 import 제거 (#34)

--- src/main/java/upbrella/be/login/dto/token/OauthToken.java
+++ src/main/java/upbrella/be/login/dto/token/OauthToken.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
