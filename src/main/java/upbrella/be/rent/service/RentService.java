e16e7228ace3cbb1a5dda5709905b6fd37e69824
acceptor-gyu <gseonggyu968@gmail.com>
2023-07-22 20:16:59 +0900

    feat: 상태 신고 내역 조회 기능 구현을 위해 유저를 가져오는 메서드 임시로 생성(#48)

--- src/main/java/upbrella/be/rent/service/RentService.java
+++ src/main/java/upbrella/be/rent/service/RentService.java
@@ -7,17 +7,26 @@ import upbrella.be.rent.dto.request.RentUmbrellaByUserRequest;
 import upbrella.be.rent.repository.RentRepository;
 import upbrella.be.umbrella.entity.Umbrella;
 import upbrella.be.umbrella.service.UmbrellaService;
+import upbrella.be.user.entity.User;
+import upbrella.be.user.service.UserService;
 
 @Service
 @RequiredArgsConstructor
 public class RentService {
 
     private final UmbrellaService umbrellaService;
+    private final UserService userService;
 
     @Transactional
     public void addRental(RentUmbrellaByUserRequest rentUmbrellaByUserRequest) {
 
         Umbrella willRentUmbrella = umbrellaService.findById(rentUmbrellaByUserRequest.getUmbrellaId());
+
+        // TODO: 추후에 세션으로 검증된 유저로 변경
+        // 임시로 mock user 사용
+        User userToRent = userService.findById(1L);
+
+
 
 
     }
