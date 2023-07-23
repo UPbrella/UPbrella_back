93156be02cafd5ab11b3d74d99b06c67ff03e639
birdieHyun <flaehdan@gmail.com>
2023-07-23 20:56:33 +0900

    test: StoreMetaService 테스트코드 작성(#55)

--- src/test/java/upbrella/be/store/service/StoreMetaServiceTest.java
+++ src/test/java/upbrella/be/store/service/StoreMetaServiceTest.java
@@ -3,55 +3,90 @@ package upbrella.be.store.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import upbrella.be.store.dto.request.CreateStoreRequest;
+import upbrella.be.store.entity.StoreDetail;
+import upbrella.be.store.entity.StoreImage;
 import upbrella.be.store.entity.StoreMeta;
+import upbrella.be.store.repository.StoreDetailRepository;
+import upbrella.be.store.repository.StoreImageRepository;
 import upbrella.be.store.repository.StoreMetaRepository;
 
-@Transactional
-@SpringBootTest
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class StoreMetaServiceTest {
 
-    @Autowired
+    @Mock
     private StoreMetaRepository storeMetaRepository;
-    @Autowired
+
+    @Mock
+    private StoreDetailRepository storeDetailRepository;
+
+    @Mock
+    private StoreImageRepository storeImageRepository;
+
+    @InjectMocks
     private StoreMetaService storeMetaService;
 
     @Test
     @DisplayName("존재하는 협업지점을 조회시 정상 조회된다.")
     void findByIdTest() {
+        StoreMeta storeMeta = StoreMeta.builder().build();
+        given(storeMetaRepository.findByIdAndDeletedIsFalse(1L)).willReturn(Optional.of(storeMeta));
+
+        storeMetaService.findById(1L);
+
+        verify(storeMetaRepository, times(1)).findByIdAndDeletedIsFalse(1L);
+    }
+
+
+    @Test
+    @DisplayName("협업지점 생성시 정상적으로 생성된다.")
+    void createStoreTest() {
         // given
-        StoreMeta storeMeta = StoreMeta.builder()
+        CreateStoreRequest request = CreateStoreRequest.builder()
                 .name("업브렐라")
                 .category("카페")
                 .classification("카페")
                 .subClassification("커피전문점")
-                .thumbnail("https://upbrella.co.kr/images/logo.png")
-                .activated(true)
+                .activateStatus(true)
+                .address("서울특별시 강남구 테헤란로 427")
+                .umbrellaLocation("1층")
+                .businessHours("09:00 ~ 18:00")
+                .contactNumber("010-0000-0000")
+                .instagramId("upbrella")
                 .latitude(37.503716)
                 .longitude(127.053718)
+                .content("업브렐라입니다.")
+                .imageUrls(
+                        List.of(
+                                "https://upbrella.s3.ap-northeast-2.amazonaws.com/umbrella-store/1/1.jpg",
+                                "https://upbrella.s3.ap-northeast-2.amazonaws.com/umbrella-store/1/2.jpg",
+                                "https://upbrella.s3.ap-northeast-2.amazonaws.com/umbrella-store/1/3.jpg"
+                        )
+                )
                 .build();
 
-        // when
-        StoreMeta savedStoreMeta = storeMetaRepository.save(storeMeta);
-        StoreMeta foundStoreMeta = storeMetaService.findById(savedStoreMeta.getId());
+        given(storeMetaRepository.save(any(StoreMeta.class))).willReturn(StoreMeta.builder().build());
+        given(storeDetailRepository.save(any(StoreDetail.class))).willReturn(StoreDetail.builder().build());
+        given(storeImageRepository.save(any(StoreImage.class))).willReturn(StoreImage.builder().build());
 
-        // then
-        Assertions.assertThat(foundStoreMeta.getId()).isEqualTo(savedStoreMeta.getId());
-    }
+        storeMetaService.createStore(request);
 
-    @Test
-    @DisplayName("존재하지 않는 협업지점을 조회시 예외가 발생한다.")
-    void test() {
-        // given
-
-
-        // when
-
-
-        // then
-        Assertions.assertThatThrownBy(() -> storeMetaService.findById(999999999L))
-                .isInstanceOf(IllegalArgumentException.class);
+        verify(storeMetaRepository, times(1)).save(any(StoreMeta.class));
+        verify(storeDetailRepository, times(1)).save(any(StoreDetail.class));
+        verify(storeImageRepository, times(request.getImageUrls().size())).save(any(StoreImage.class));
     }
 }
