c718e00286bc57ea51f396821f35301de1f135cb
birdieHyun <flaehdan@gmail.com>
2023-07-24 17:56:56 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/upbrella/be/store/repository/StoreImageRepository.java
+++ src/main/java/upbrella/be/store/repository/StoreImageRepository.java
@@ -1,8 +1,6 @@
 package upbrella.be.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import upbrella.be.store.entity.StoreImage;
 
 import java.util.List;
