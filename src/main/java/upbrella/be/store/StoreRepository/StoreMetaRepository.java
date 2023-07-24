93b55805593935d3814b0a20f6aac77d4f8d0605
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-07-24 22:49:08 +0900

    [feat] 현재 위치(지도) 기준 협업 지점 조회 기능 구현 및 테스트 코드 작성 (#70) (#73)
    * feat: 현재 지도 기준 협업 지점 목록 조회 기능 구현 및 Controller Test 수정, API도 일부 수정 있음
    
    * feat: 현재 지도 기준 협업 지점 목록 조회 기능 서비스 테스트 작성
    
    * chore: 테스트 필수항목 추가

--- src/main/java/upbrella/be/store/StoreRepository/StoreMetaRepository.java
+++ src/main/java/upbrella/be/store/StoreRepository/StoreMetaRepository.java
@@ -3,8 +3,10 @@ package upbrella.be.store.StoreRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import upbrella.be.store.entity.StoreMeta;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreMetaRepository extends JpaRepository<StoreMeta, Long> {
     Optional<StoreMeta> findByIdAndDeletedIsFalse(long id);
+    List<StoreMeta> findAllByDeletedIsFalseAndLatitudeBetweenAndLongitudeBetween(double latitudeFrom, double latitudeTo, double longitudeFrom, double longitudeTo);
 }
