ca3f8cae07a31f998a219712e30189d4ccd47a0b
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-07-26 15:37:19 +0900

    [feat] 로그인한 사용자 정보 조회, 로그인한 사용자가 대여 중인 우산 정보 조회 구현 및 테스트 작성 (#74, #75) (#78)
    * feat: 세션을 통해 user를 받아왔다고 가정하고  builder로 유저를 생성함(#74)
    
    * feat: 필요없는 멤버 변수 삭제 및 Response 생성 로직 이동
    
    * feat: response에서 사용하지 않는 멤버 변수 삭제(#74)
    
    * feat: 로그인한 사용자가 대여 중인 우산 정보 조회 기능 구현을 위한 메서드 생성(#75)
    
    * chore: 이전에 구현한 기능에 필요한 로직 TODO 주석으로 추가(#75)
    
    * feat: 대여 중인 우산이 있는지 체크 후 반환하도록 함(#75)
    
    * feat: 로그인한 사용자가 대여한 우산 조회 기능 구현 중 추가된 로직에 따라 테스트 추가(#75)
    
    * chore: 변수명 수정(#75)
    
    * chore: 테스트 코드에서 mock 객체 어노테이션으로 주입하도록 수정(#75)

--- src/main/java/upbrella/be/user/controller/UserController.java
+++ src/main/java/upbrella/be/user/controller/UserController.java
@@ -5,8 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import upbrella.be.rent.entity.History;
+import upbrella.be.rent.repository.RentRepository;
+import upbrella.be.umbrella.repository.UmbrellaRepository;
 import upbrella.be.user.dto.response.UmbrellaBorrowedByUserResponse;
 import upbrella.be.user.dto.response.UserInfoResponse;
+import upbrella.be.user.entity.User;
 import upbrella.be.user.service.UserService;
 import upbrella.be.util.CustomResponse;
 
@@ -18,25 +22,54 @@ import javax.servlet.http.HttpSession;
 public class UserController {
 
     private final UserService userService;
+    private final RentRepository rentRepository;
 
     @GetMapping
     public ResponseEntity<CustomResponse<UserInfoResponse>> findUserInfo(HttpSession httpSession) {
+
+        /**
+         * TODO: 세션 처리으로 로그인한 유저의 id 가져오기
+         *       interceptor에서 userId를 가져올 것인지, user 객체를 가져올 것인지 논의 후 구현
+         */
+
+        // session에서 꺼낸 것이라는 의미로 repository로부터 findById를 하지 않고 빌더를 사용해서 만들었음.
+        User loggedInUser = User.builder()
+                .id(1L)
+                .name("사용자")
+                .phoneNumber("010-0000-0000")
+                .adminStatus(false)
+                .build();
+
         return ResponseEntity
                 .ok()
                 .body(new CustomResponse<>(
                         "success",
                         200,
                         "로그인 유저 정보 조회 성공",
-                        UserInfoResponse.builder()
-                                .id(1)
-                                .name("사용자")
-                                .phoneNumber("010-0000-0000")
-                                .adminStatus(false)
-                                .build()));
+                        UserInfoResponse.fromUser(loggedInUser)));
     }
 
     @GetMapping("/umbrella")
     public ResponseEntity<CustomResponse<UmbrellaBorrowedByUserResponse>> findUmbrellaBorrowedByUser(HttpSession httpSession) {
+
+        /**
+         * TODO: 세션 처리으로 로그인한 유저의 id 가져오기
+         *       interceptor에서 userId를 가져올 것인지, user 객체를 가져올 것인지 논의 후 구현
+         */
+
+        // session에서 꺼낸 것이라는 의미로 repository로부터 findById를 하지 않고 빌더를 사용해서 만들었음.
+        User loggedInUser = User.builder()
+                .id(72L)
+                .name("사용자")
+                .phoneNumber("010-1234-5678")
+                .adminStatus(false)
+                .build();
+
+        History rentalHistory = rentRepository.findByUserAndReturnedAtIsNull(loggedInUser.getId())
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 사용자가 빌린 우산이 없습니다."));
+
+        long borrowedUmbrellaUuid = rentalHistory.getUmbrella().getUuid();
+
         return ResponseEntity
                 .ok()
                 .body(new CustomResponse<>(
@@ -44,7 +77,7 @@ public class UserController {
                         200,
                         "사용자가 빌린 우산 조회 성공",
                         UmbrellaBorrowedByUserResponse.builder()
-                                .id(1L)
+                                .uuid(borrowedUmbrellaUuid)
                                 .build()));
     }
 }
