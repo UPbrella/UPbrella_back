9c8496d0cb1899555181dee02097b73705b6d1d8
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-07-26 16:19:59 +0900

    Revert "[feat] 로그인한 사용자 정보 조회, 로그인한 사용자가 대여 중인 우산 정보 조회 구현 및 테스트 작성 (#74, #75) (#78)" (#81)
    This reverts commit ca3f8cae07a31f998a219712e30189d4ccd47a0b.

--- src/main/java/upbrella/be/user/dto/response/UserInfoResponse.java
+++ src/main/java/upbrella/be/user/dto/response/UserInfoResponse.java
@@ -3,7 +3,6 @@ package upbrella.be.user.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import upbrella.be.user.entity.User;
 
 @Getter
 @Setter
@@ -13,12 +12,5 @@ public class UserInfoResponse {
     private long id;
     private String name;
     private String phoneNumber;
-
-    public static UserInfoResponse fromUser(User user) {
-        return UserInfoResponse.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .phoneNumber(user.getPhoneNumber())
-                .build();
-    }
+    private boolean adminStatus;
 }
