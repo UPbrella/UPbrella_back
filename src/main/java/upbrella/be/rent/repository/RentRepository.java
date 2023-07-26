158c6a8c8771a46758b2923d0bca90147391d225
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-07-26 15:37:19 +0900

    [feat] 로그인한 사용자 정보 조회, 로그인한 사용자가 대여 중인 우산 정보 조회 구현 및 테스트 작성 (#74, #75) (#78)
    * feat: 세션을 통해 user를 받아왔다고 가정하고  builder로 유저를 생성함(#74)
    
    * feat: 필요없는 멤버 변수 삭제 및 Response 생성 로직 이동
    
    * feat: response에서 사용하지 않는 멤버 변수 삭제(#74)
    
    * feat: 로그인한 사용자가 대여 중인 우산 정보 조회 기능 구현을 위한 메서드 생성(#75)
    
    * chore: 이전에 구현한 기능에 필요한 로직 TODO 주석으로 추가(#75)
    
    * feat: 대여 중인 우산이 있는지 체크 후 반환하도록 함(#75)
    
    * feat: 로그인한 사용자가 대여한 우산 조회 기능 구현 중 추가된 로직에 따라 테스트 추가(#75)
    
    * chore: 변수명 수정(#75)
    
    * chore: 테스트 코드에서 mock 객체 어노테이션으로 주입하도록 수정(#75)

--- src/main/java/upbrella/be/rent/repository/RentRepository.java
+++ src/main/java/upbrella/be/rent/repository/RentRepository.java
@@ -3,6 +3,9 @@ package upbrella.be.rent.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import upbrella.be.rent.entity.History;
 
+import java.util.Optional;
+
 public interface RentRepository extends JpaRepository<History, Long> {
 
+    Optional<History> findByUserAndReturnedAtIsNull(Long userId);
 }
