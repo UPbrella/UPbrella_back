825e9e71a5aaf91219fb081dd56c3bab4aafaf23
birdieHyun <flaehdan@gmail.com>
2023-08-01 19:50:32 +0900

    refactor: 사용하지 않는 import 제거(#55)

--- src/main/java/upbrella/be/store/service/StoreImageService.java
+++ src/main/java/upbrella/be/store/service/StoreImageService.java
@@ -15,7 +15,6 @@ import upbrella.be.store.repository.StoreDetailRepository;
 import upbrella.be.store.repository.StoreImageRepository;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 @Service
