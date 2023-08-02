fc34d39f15d3e0f0808c4bcd986b8cbbea5a843a
gwonwoo-nam <ngw7617@naver.com>
2023-08-03 01:29:42 +0900

    chore: 인가 인터셉터 모든 메서드에 대해 동작하도록 변경

--- src/main/java/upbrella/be/error/controller/ErrorController.java
+++ src/main/java/upbrella/be/error/controller/ErrorController.java
@@ -1,15 +1,19 @@
 package upbrella.be.error.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import upbrella.be.util.CustomResponse;
 
 @RestController
 public class ErrorController {
 
-    @GetMapping(value = "/api/error")
-    //@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @RequestMapping(value = "/api/error",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ResponseEntity<CustomResponse> getError() {
 
         return ResponseEntity
