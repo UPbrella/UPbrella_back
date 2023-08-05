cfd332550859952b77179db1f63fe722d1100626
acceptor-gyu <gseonggyu968@gmail.com>
2023-08-05 21:35:10 +0900

    fix: 무분별한 어노테이션 삭제(#47)

--- src/main/java/upbrella/be/rent/service/RentService.java
+++ src/main/java/upbrella/be/rent/service/RentService.java
@@ -44,7 +44,6 @@ public class RentService {
         );
     }
 
-    @Transactional
     public RentalHistoriesPageResponse findAllHistories(HistoryFilterRequest filter) {
 
         return RentalHistoriesPageResponse.of(findAllRentalHistory(filter));
