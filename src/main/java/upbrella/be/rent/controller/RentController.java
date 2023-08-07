f154256ccec16367c9c1161d5d7a58bc79d5ea01
acceptor-gyu <gseonggyu968@gmail.com>
2023-08-07 13:55:37 +0900

    chore: upstream 병합 중 충돌 해결(#49)

--- src/main/java/upbrella/be/rent/controller/RentController.java
+++ src/main/java/upbrella/be/rent/controller/RentController.java
@@ -60,15 +60,9 @@ public class RentController {
     }
 
     @GetMapping("/histories")
-<<<<<<< HEAD
     public ResponseEntity<CustomResponse<RentalHistoriesPageResponse>> findRentalHistory(HistoryFilterRequest filter, HttpSession httpSession) {
 
         RentalHistoriesPageResponse histories = rentService.findAllHistories(filter);
-=======
-    public ResponseEntity<CustomResponse<RentalHistoriesPageResponse>> findRentalHistory(HistoryFilterRequest historyFilterRequest, HttpSession httpSession) {
-
-        RentalHistoriesPageResponse allHistories = rentService.findAllHistories(historyFilterRequest);
->>>>>>> dev
 
         return ResponseEntity
                 .ok()
@@ -76,11 +70,7 @@ public class RentController {
                         "success",
                         200,
                         "대여 내역 조회 성공",
-<<<<<<< HEAD
                         histories
-=======
-                        allHistories
->>>>>>> dev
                 ));
     }
 
