a15d8b57c5c80ff790d978707522965e4afb0c40
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-08-22 16:29:58 +0900

    [feat] 개발 서버 배포 CI/CD 스크립트 업데이트 (#167)
    * chore: build script 초안 작성 테스트
    
    * chore: build script 초안 작성 테스트
    
    * chore: build script 초안 작성 테스트
    
    * chore: build script 초안 작성 테스트
    
    * chore: build script 초안 작성 테스트
    
    * chore: build script 초안 작성 테스트
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Create Dockerfile
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * Update dev-release-ci.yml
    
    * chore: 불필요 파일 삭제
    
    * Update and rename dev-release-ci.yml to release-production-ci.yml
    
    * Create release-dev-ci.yml
    
    * Update release-dev-ci.yml
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update release-dev-ci.yml

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-container_name=upbrella-server-dev
-
 REPOSITORY=/opt/testapp
+APP_NAME=upbrella-dev-deploy
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep java -fl | awk '{print $1}')
 
 if [ -z $CURRENT_PID ]
 then
@@ -21,4 +19,4 @@ fi
 echo "> $JAR_PATH 배포"
 
 
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
