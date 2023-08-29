30692458cb828348cee5b936e364d90d4c29fc71
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-08-29 16:00:59 +0900

    [feat] 우산 반납 시 슬랙 봇 알람 기능 (#184) (#185)
    * feat: 반납 시 Slack 알림 기능 구현
    
    * feat: 반납 시 개인정보 유출 없이 알림 기능 추가
    
    * feat: 슬랙 봇 알림 #업브렐라-운영-알림-봇으로 전송 및 URL property 설정

--- src/main/java/upbrella/be/rent/repository/RentRepository.java
+++ src/main/java/upbrella/be/rent/repository/RentRepository.java
@@ -13,4 +13,5 @@ public interface RentRepository extends JpaRepository<History, Long>, RentReposi
 
     long countByRentStoreMetaId(long storeId);
 
+    long countAllByReturnedAtIsNotNullAndPaidAtIsNotNullAndRefundedAtIsNull();
 }
