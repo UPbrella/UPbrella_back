c16a34a11a50454e3bb4336b6689abcf1099ff8b
acceptor-gyu <gseonggyu968@gmail.com>
2023-08-30 12:20:02 +0900

    feat: 한 번에 하나의 우산만 빌릴 수 있도록 예외 처리 추가 (#146)

--- src/main/java/upbrella/be/rent/controller/RentExceptionHandler.java
+++ src/main/java/upbrella/be/rent/controller/RentExceptionHandler.java
@@ -3,6 +3,7 @@ package upbrella.be.rent.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import upbrella.be.rent.exception.ExistingUmbrellaForRentException;
 import upbrella.be.rent.exception.NonExistingHistoryException;
 import upbrella.be.rent.exception.NonExistingUmbrellaForRentException;
 import upbrella.be.store.exception.NonExistingClassificationException;
@@ -32,6 +33,18 @@ public class RentExceptionHandler {
                         "fail",
                         400,
                         "해당 대여 기록이 없습니다."
+                ));
+    }
+
+    @ExceptionHandler(ExistingUmbrellaForRentException.class)
+    public ResponseEntity<CustomErrorResponse> existingUmbrellaForRent(ExistingUmbrellaForRentException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(new CustomErrorResponse(
+                        "fail",
+                        400,
+                        "이미 대여 중인 우산이 있습니다."
                 ));
     }
 }
