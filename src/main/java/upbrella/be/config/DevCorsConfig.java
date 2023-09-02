ec9c11af830853548205f5339b42d31afa78cee5
gwonwoo-nam <ngw7617@naver.com>
2023-09-02 19:06:36 +0900

    test: Cors 설정 Filter 변경 테스트

--- src/main/java/upbrella/be/config/DevCorsConfig.java
+++ src/main/java/upbrella/be/config/DevCorsConfig.java
@@ -21,12 +21,17 @@ public class DevCorsConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-
-        if (request.getHeader("Host").contains("upbrella-dev.site")) {
-            response.setHeader("Access-Control-Allow-Origin", "http://upbrella-dev.site");
-        } else {
+        if (request.getHeader("Origin") != null) {
+            if (request.getHeader("Origin").contains("upbrella-dev.site")) {
+                response.setHeader("Access-Control-Allow-Origin", "http://upbrella-dev.site");
+            } else {
+                response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            }
+        }
+        else {
             response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         }
+
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, HEAD, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "10");
