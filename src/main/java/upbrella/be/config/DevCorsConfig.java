e19cd54a7855e9e582aa3423e5ea3fd4338b5122
gwonwoo-nam <ngw7617@naver.com>
2023-09-02 17:04:25 +0900

    test: Config 설정 테스트

---
+++ src/main/java/upbrella/be/config/DevCorsConfig.java
@@ -0,0 +1,23 @@
+package upbrella.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class DevCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        //TODO: CORS 세부 설정 예정
+        registry.addMapping("/**")
+                .allowedOrigins("http://upbrella-dev.site")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name())
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
