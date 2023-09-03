c79918dca132b876168e7fe1bbfbeb986c8c62b0
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-09-03 10:27:38 +0900

    [fix] CORS 에러 수정 (#204)
    * test: Config 설정 테스트
    
    * chore: 주석 제거
    
    * test: Cors 설정 Filter 변경 테스트
    
    * test: Cors 설정 Filter 변경 테스트
    
    * test: Cors 설정 Filter 변경 테스트
    
    ---------
    
    Co-authored-by: 임동현 <flaehdan@gmail.com>

--- src/main/java/upbrella/be/config/ProductionCorsConfig.java
+++ src/main/java/upbrella/be/config/ProductionCorsConfig.java
@@ -1,19 +1,21 @@
 package upbrella.be.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@Profile("prod")
+public class ProductionCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        //TODO: CORS 세부 설정 예정
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://upbrella-dev.site:80")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedOrigins("https://upbrella.co.kr")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name())
                 .allowCredentials(true)
                 .allowedHeaders("*");
     }
