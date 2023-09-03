a686ef0b749bcf60845391b92fef383534c99d57
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-09-03 10:27:38 +0900

    [fix] CORS 에러 수정 (#204)
    * test: Config 설정 테스트
    
    * chore: 주석 제거
    
    * test: Cors 설정 Filter 변경 테스트
    
    * test: Cors 설정 Filter 변경 테스트
    
    * test: Cors 설정 Filter 변경 테스트
    
    ---------
    
    Co-authored-by: 임동현 <flaehdan@gmail.com>

---
+++ src/main/java/upbrella/be/config/DevCorsConfig.java
@@ -0,0 +1,48 @@
+package upbrella.be.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Profile("dev")
+@Component
+public class DevCorsConfig implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        if (request.getHeader("Origin") != null) {
+            if (request.getHeader("Origin").contains("upbrella-dev.site")) {
+                response.setHeader("Access-Control-Allow-Origin", "http://upbrella-dev.site");
+            } else {
+                response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            }
+        }
+        else {
+            response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        }
+
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, HEAD, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "10");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
