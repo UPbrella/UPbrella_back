2f8751a2885be340fdd7a78332a3f655a3faa9c5
birdieHyun <flaehdan@gmail.com>
2023-09-19 09:09:13 +0900

    refactor: 암호화 방싱 ECB에서 CBC로 변경 (#275)

--- src/main/java/upbrella/be/util/AesEncryptor.java
+++ src/main/java/upbrella/be/util/AesEncryptor.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 @Component
@@ -34,9 +36,20 @@ public class AesEncryptor {
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             SecretKeySpec secretKey = new SecretKeySpec(KEY, "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            byte[] iv = new byte[16]; // IV length should be 16 bytes for AES
+            new SecureRandom().nextBytes(iv);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
             encryptedBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-        }catch (Exception e) {
+
+            byte[] finalData = new byte[iv.length + encryptedBytes.length];
+            System.arraycopy(iv, 0, finalData, 0, iv.length);
+            System.arraycopy(encryptedBytes, 0, finalData, iv.length, encryptedBytes.length);
+
+            encryptedBytes = finalData;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return Base64.getEncoder().encodeToString(encryptedBytes);
@@ -52,9 +65,16 @@ public class AesEncryptor {
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             SecretKeySpec secretKey = new SecretKeySpec(KEY, "AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
-        }catch (Exception e) {
+
+            byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+            byte[] iv = new byte[16]; // extracting IV from encrypted data
+            System.arraycopy(decodedData, 0, iv, 0, 16);
+
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+
+            decryptedBytes = cipher.doFinal(decodedData, 16, decodedData.length - 16);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new String(decryptedBytes, StandardCharsets.UTF_8);
