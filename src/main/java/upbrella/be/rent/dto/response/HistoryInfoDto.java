ee816c5227a6de17ca6c1d1c9eb71826d61c68f9
임동현 <flaehdan@gmail.com>
2023-09-21 21:01:51 +0900

    [refactor] 대여 기록 조회 api 성능 개선 (#300) (#301)
    * refactor: 대여 기록 조회 api 성능 개선 (#300)
    
    * refactor: 패키지 구조 수정 (#300)
    
    * refactor: 코드를 간결하게 수정 (#300)

---
+++ src/main/java/upbrella/be/rent/dto/response/HistoryInfoDto.java
@@ -0,0 +1,44 @@
+package upbrella.be.rent.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class HistoryInfoDto {
+
+    private long id;
+    private String name;
+    private String phoneNumber;
+    private String rentStoreName;
+    private LocalDateTime rentAt;
+    private long umbrellaUuid;
+    private String returnStoreName;
+    private LocalDateTime returnAt;
+    private LocalDateTime paidAt;
+    private String bank;
+    private String accountNumber;
+    private String etc;
+    private LocalDateTime refundedAt;
+
+    @QueryProjection
+    public HistoryInfoDto(long id, String name, String phoneNumber, String rentStoreName, LocalDateTime rentAt, long umbrellaUuid, String returnStoreName, LocalDateTime returnAt, LocalDateTime paidAt, String bank, String accountNumber, String etc, LocalDateTime refundedAt) {
+
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.rentStoreName = rentStoreName;
+        this.rentAt = rentAt;
+        this.umbrellaUuid = umbrellaUuid;
+        this.returnStoreName = returnStoreName;
+        this.returnAt = returnAt;
+        this.paidAt = paidAt;
+        this.bank = bank;
+        this.accountNumber = accountNumber;
+        this.etc = etc;
+        this.refundedAt = refundedAt;
+    }
+}
