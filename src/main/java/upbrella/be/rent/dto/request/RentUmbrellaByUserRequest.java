2d06940eec7bdfb202a1876de8d137a161dcda4f
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-10-02 15:13:33 +0900

    fix: 상태 신고, 개선 사항 요청 글자수 제한 (#314) (#318)

--- src/main/java/upbrella/be/rent/dto/request/RentUmbrellaByUserRequest.java
+++ src/main/java/upbrella/be/rent/dto/request/RentUmbrellaByUserRequest.java
@@ -2,6 +2,8 @@ package upbrella.be.rent.dto.request;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -11,5 +13,7 @@ public class RentUmbrellaByUserRequest {
     private String region;
     private long storeId;
     private long umbrellaId;
+
+    @Size(max = 400, message = "conditionReport는 최대 400자여야 합니다.")
     private String conditionReport;
 }
