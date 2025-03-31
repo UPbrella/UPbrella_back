47e149fdbde66097dfc054fba173946dd8b93afa
birdie <flaehdan@gmail.com>
2025-03-31 21:42:35 +0900

    refactor: report kotlin 마이그레이션 (#458)

--- src/main/java/upbrella/be/rent/service/RentService.java
+++ src/main/java/upbrella/be/rent/service/RentService.java
@@ -56,14 +56,16 @@ public class RentService {
         return RentFormResponse.of(umbrella);
     }
 
-    public ReturnFormResponse findReturnForm(long storeId, User userToReturn, String salt, String signature) {
+    public ReturnFormResponse findReturnForm(long storeId, User userToReturn, String salt,
+        String signature) {
 
         StoreMeta storeMeta = storeMetaService.findStoreMetaById(storeId);
 
         lockerService.validateLockerSignature(storeMeta.getId(), salt, signature);
 
         History history = rentRepository.findByUserIdAndReturnedAtIsNull(userToReturn.getId())
-                .orElseThrow(() -> new NonExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 없습니다."));
+            .orElseThrow(
+                () -> new NonExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 없습니다."));
 
         return ReturnFormResponse.of(storeMeta, history);
     }
@@ -73,33 +75,33 @@ public class RentService {
         userService.checkBlackList(userToRent.getId());
 
         rentRepository.findByUserIdAndReturnedAtIsNull(userToRent.getId())
-                .ifPresent(history -> {
-                    throw new ExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 있습니다.");
-                });
+            .ifPresent(history -> {
+                throw new ExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 있습니다.");
+            });
 
-        Umbrella willRentUmbrella = umbrellaService.findUmbrellaById(rentUmbrellaByUserRequest.getUmbrellaId());
-        if(willRentUmbrella.getStoreMeta().getId() != rentUmbrellaByUserRequest.getStoreId()){
+        Umbrella willRentUmbrella = umbrellaService.findUmbrellaById(
+            rentUmbrellaByUserRequest.getUmbrellaId());
+        if (willRentUmbrella.getStoreMeta().getId() != rentUmbrellaByUserRequest.getStoreId()) {
             throw new UmbrellaStoreMissMatchException("[ERROR] 해당 우산은 해당 매장에 존재하지 않습니다.");
         }
         if (willRentUmbrella.getMissed()) {
             throw new MissingUmbrellaException("[ERROR] 해당 우산은 분실되었습니다.");
         }
-        if (!willRentUmbrella.getRentable()){
+        if (!willRentUmbrella.getRentable()) {
             throw new NotAvailableUmbrellaException("[ERROR] 해당 우산은 대여중입니다.");
         }
 
         willRentUmbrella.rentUmbrella();
-        StoreMeta rentalStore = storeMetaService.findStoreMetaById(rentUmbrellaByUserRequest.getStoreId());
+        StoreMeta rentalStore = storeMetaService.findStoreMetaById(
+            rentUmbrellaByUserRequest.getStoreId());
 
         String conditionReport = rentUmbrellaByUserRequest.getConditionReport();
 
         History history = rentRepository.save(
             History.ofCreatedByNewRent(willRentUmbrella, userToRent, rentalStore));
 
-        ConditionReport conditionReportToSave = ConditionReport.builder()
-                .content(conditionReport)
-                .history(history)
-                .build();
+        ConditionReport conditionReportToSave = new ConditionReport(
+            history, conditionReport, null, null);
 
         conditionReportService.saveConditionReport(conditionReportToSave);
     }
@@ -112,7 +114,8 @@ public class RentService {
 
         userService.checkBlackList(userToReturn.getId());
         History history = rentRepository.findByUserIdAndReturnedAtIsNull(userToReturn.getId())
-                .orElseThrow(() -> new NonExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 없습니다."));
+            .orElseThrow(
+                () -> new NonExistingUmbrellaForRentException("[ERROR] 해당 유저가 대여 중인 우산이 없습니다."));
 
         StoreMeta returnStore = storeMetaService.findStoreMetaById(request.getReturnStoreId());
 
@@ -125,12 +128,14 @@ public class RentService {
     }
 
     @Transactional
-    public RentalHistoriesPageResponse findAllHistories(HistoryFilterRequest filter, Pageable pageable) {
+    public RentalHistoriesPageResponse findAllHistories(HistoryFilterRequest filter,
+        Pageable pageable) {
 
         long countOfAllHistories = rentRepository.countAll(filter, pageable);
         long countOfAllPages = countOfAllHistories / pageable.getPageSize();
 
-        return RentalHistoriesPageResponse.of(findAllRentalHistory(filter, pageable), countOfAllHistories, countOfAllPages);
+        return RentalHistoriesPageResponse.of(findAllRentalHistory(filter, pageable),
+            countOfAllHistories, countOfAllPages);
     }
 
     public AllHistoryResponse findAllHistoriesByUser(long userId) {
@@ -138,21 +143,23 @@ public class RentService {
         return AllHistoryResponse.of(findAllByUserId(userId));
     }
 
-    private void addImprovementReportFromReturnByUser(History history, ReturnUmbrellaByUserRequest request) {
+    private void addImprovementReportFromReturnByUser(History history,
+        ReturnUmbrellaByUserRequest request) {
 
         if (request.getImprovementReportContent() == null) {
             return;
         }
 
-        improvementReportService.addImprovementReportFromReturn(history, request.getImprovementReportContent());
+        improvementReportService.addImprovementReportFromReturn(history,
+            request.getImprovementReportContent());
     }
 
     private List<SingleHistoryResponse> findAllByUserId(long userId) {
 
         return findAllByUser(userId)
-                .stream()
-                .map(this::toSingleHistoryResponse)
-                .collect(Collectors.toList());
+            .stream()
+            .map(this::toSingleHistoryResponse)
+            .collect(Collectors.toList());
     }
 
     private List<History> findAllByUser(long userId) {
@@ -160,11 +167,12 @@ public class RentService {
         return rentRepository.findAllByUserId(userId);
     }
 
-    private List<RentalHistoryResponse> findAllRentalHistory(HistoryFilterRequest filter, Pageable pageable) {
+    private List<RentalHistoryResponse> findAllRentalHistory(HistoryFilterRequest filter,
+        Pageable pageable) {
 
         return findHistoryInfos(filter, pageable).stream()
-                .map(this::toRentalHistoryResponse)
-                .collect(Collectors.toList());
+            .map(this::toRentalHistoryResponse)
+            .collect(Collectors.toList());
     }
 
     private SingleHistoryResponse toSingleHistoryResponse(History history) {
@@ -193,7 +201,8 @@ public class RentService {
         if (history.getReturnAt() != null) {
 
             elapsedDay = history.getReturnAt().getDayOfYear() - history.getRentAt().getDayOfYear();
-            totalRentalDay = history.getReturnAt().getDayOfYear() - history.getRentAt().getDayOfYear();
+            totalRentalDay =
+                history.getReturnAt().getDayOfYear() - history.getRentAt().getDayOfYear();
 
             return RentalHistoryResponse.createReturnedHistory(history, elapsedDay, totalRentalDay);
         }
@@ -240,13 +249,14 @@ public class RentService {
     private History findHistoryById(long historyId) {
 
         return rentRepository.findById(historyId)
-                .orElseThrow(() -> new NonExistingHistoryException("[ERROR] 해당 대여 기록이 없습니다."));
+            .orElseThrow(() -> new NonExistingHistoryException("[ERROR] 해당 대여 기록이 없습니다."));
     }
 
     public History findRentalHistoryByUser(SessionUser sessionUser) {
 
         return rentRepository.findByUserIdAndReturnedAtIsNull(sessionUser.getId())
-                .orElseThrow(() -> new NonExistingBorrowedHistoryException("[ERROR] 사용자가 빌린 우산이 없습니다."));
+            .orElseThrow(
+                () -> new NonExistingBorrowedHistoryException("[ERROR] 사용자가 빌린 우산이 없습니다."));
     }
 
     @Transactional
---
+++ src/main/kotlin/upbrella/be/rent/entity/ConditionReport.kt
@@ -0,0 +1,15 @@
+package upbrella.be.rent.entity
+
+import javax.persistence.*
+
+@Entity
+class ConditionReport(
+    @OneToOne
+    @JoinColumn(name = "history_id")
+    val history: History? = null,
+    val content: String? = null,
+    val etc: String? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+)
