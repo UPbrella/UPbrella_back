e97bd42499d2427a691f332aff2a916749133561
gwonwoo-nam <ngw7617@naver.com>
2025-04-05 00:41:41 +0900

    refactor: umbrella-dto kotlin migration

---
+++ src/main/kotlin/upbrella/be/umbrella/dto/request/UmbrellaCreateRequest.kt
@@ -0,0 +1,9 @@
+package upbrella.be.umbrella.dto.request;
+
+data class UmbrellaCreateRequest (
+
+    val storeMetaId: Long,
+    val uuid: Long,
+    val rentable: Boolean,
+    val etc: String?
+)
---
+++ src/main/kotlin/upbrella/be/umbrella/dto/request/UmbrellaModifyRequest.kt
@@ -0,0 +1,10 @@
+package upbrella.be.umbrella.dto.request;
+
+data class UmbrellaModifyRequest (
+
+    val storeMetaId: Long,
+    val uuid: Long,
+    val rentable: Boolean,
+    val missed: Boolean,
+    val etc: String
+)
