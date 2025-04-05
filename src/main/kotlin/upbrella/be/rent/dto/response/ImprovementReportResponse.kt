da382346a300f1bbb49db34f853c3f8ec9cb4700
birdie <flaehdan@gmail.com>
2025-04-05 12:02:33 +0900

    refactor: rent dto migrate

---
+++ src/main/kotlin/upbrella/be/rent/dto/response/ImprovementReportResponse.kt
@@ -0,0 +1,21 @@
+package upbrella.be.rent.dto.response
+
+import upbrella.be.rent.entity.ImprovementReport
+
+data class ImprovementReportResponse(
+    val id: Long = 0,
+    val umbrellaUuid: Long = 0,
+    val content: String? = "",
+    val etc: String? = ""
+) {
+    companion object {
+        fun fromImprovementReport(improvementReport: ImprovementReport): ImprovementReportResponse {
+            return ImprovementReportResponse(
+                id = improvementReport.history!!.id!!,
+                umbrellaUuid = improvementReport.history.umbrella.uuid,
+                content = improvementReport.content,
+                etc = improvementReport.etc
+            )
+        }
+    }
+}
