2aadc6c727199459c73bec76c08a3d916cf3f1e8
birdie <flaehdan@gmail.com>
2025-04-12 14:48:43 +0900

    chore: 사용되지 않는 의존성 제거

--- src/main/kotlin/upbrella/be/config/AuthConfig.kt
+++ src/main/kotlin/upbrella/be/config/AuthConfig.kt
@@ -1,6 +1,5 @@
 package upbrella.be.config
 
-import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
