781ba704aed0d1ebe4d483337a7b0759f062221b
gwonwoo-nam <ngw7617@naver.com>
2025-05-23 23:49:48 +0900

    feat: init sql

--- src/main/kotlin/upbrella/be/config/DevCorsConfig.kt
+++ src/main/kotlin/upbrella/be/config/DevCorsConfig.kt
@@ -1,43 +1,32 @@
 package upbrella.be.config
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.stereotype.Component
-import javax.servlet.*
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Profile("dev")
-@Component
-class DevCorsConfig : Filter {
+@Configuration
+class DevCorsConfig : WebMvcConfigurer {
 
-    override fun init(filterConfig: FilterConfig) {
-        // No initialization needed
-    }
-
-    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
-        val request = req as HttpServletRequest
-        val response = res as HttpServletResponse
-
-        if (request.getHeader("Origin") != null) {
-            if (request.getHeader("Origin").contains("upbrella-dev.site")) {
-                response.setHeader("Access-Control-Allow-Origin", "http://upbrella-dev.site")
-            } else {
-                response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-            }
-        } else {
-            response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-        }
-
-        response.setHeader("Access-Control-Allow-Credentials", "true")
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, HEAD, OPTIONS")
-        response.setHeader("Access-Control-Max-Age", "10")
-        response.setHeader("Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-
-        chain.doFilter(request, response)
-    }
-
-    override fun destroy() {
-        // No cleanup needed
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://upbrella-front.vercel.app"
+            )
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PATCH.name,
+                HttpMethod.DELETE.name,
+                HttpMethod.HEAD.name,
+                HttpMethod.OPTIONS.name
+            )
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
     }
 }
