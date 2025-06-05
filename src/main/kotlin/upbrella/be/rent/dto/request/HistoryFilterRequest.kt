f7e592bc580bd4add6b5769f2b292de644f5d5cc
oliver.green <ngw7617@naver.com>
2025-06-06 01:00:16 +0900

    feat: add branch and payment filters

--- src/main/kotlin/upbrella/be/rent/dto/request/HistoryFilterRequest.kt
+++ src/main/kotlin/upbrella/be/rent/dto/request/HistoryFilterRequest.kt
@@ -1,5 +1,7 @@
 package upbrella.be.rent.dto.request
 
 data class HistoryFilterRequest(
-    val refunded: Boolean? = null
+    val refunded: Boolean? = null,
+    val storeId: Long? = null,
+    val paid: Boolean? = null
 )
