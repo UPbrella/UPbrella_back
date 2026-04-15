3bbe9189ec18d35cd060760ec02529d92e209245
birdie-rim <birdie.rim@cupix.com>
2026-04-15 21:07:06 +0900

    chore: session cookie 설정 변경

--- src/main/kotlin/upbrella/be/config/SessionCookieConfig.kt
+++ src/main/kotlin/upbrella/be/config/SessionCookieConfig.kt
@@ -4,6 +4,7 @@ import org.apache.tomcat.util.http.LegacyCookieProcessor
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.boot.web.servlet.ServletContextInitializer
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -26,12 +27,16 @@ class SessionCookieConfig {
     }
 
     @Bean
+    fun cookieSameSiteSupplier(): CookieSameSiteSupplier {
+        return CookieSameSiteSupplier.ofNone()
+    }
+
+    @Bean
     fun servletContextInitializer(): ServletContextInitializer {
         return ServletContextInitializer { servletContext ->
             val sessionCookieConfig: SessionCookieConfig = servletContext.sessionCookieConfig
             sessionCookieConfig.setSecure(true)
             sessionCookieConfig.setHttpOnly(true)
-            sessionCookieConfig.setDomain(".upbrella.co.kr")
             sessionCookieConfig.setPath("/")
         }
     }
